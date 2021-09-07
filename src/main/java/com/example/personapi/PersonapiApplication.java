b3ff1d952b850e4a4293fb292e5a1ad75fc2a3dd
Maike Rodrigues <maikedev7@gmail.com>
2021-09-07 16:49:55 -0300

    Update Files

--- src/main/java/com/example/personapi/PersonapiApplication.java
+++ src/main/java/com/example/personapi/PersonapiApplication.java
@@ -1,4 +1,4 @@
-package com.personapi.personapi;
+package com.example.personapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
