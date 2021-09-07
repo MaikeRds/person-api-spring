b51e4d91e9bf70401c73e13271ea891486793afd
Maike Rodrigues <maikedev7@gmail.com>
2021-09-07 19:14:03 -0300

    Developed person create operation

---
+++ src/main/java/com/example/personapi/service/PersonService.java
@@ -0,0 +1,23 @@
+package com.example.personapi.service;
+
+import com.example.personapi.dto.response.MessageResponseDTO;
+import com.example.personapi.entity.Person;
+import com.example.personapi.repository.PersonRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    private PersonRepository personRepository;
+
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public MessageResponseDTO createPerson(Person person){
+        Person savedPerson = this.personRepository.save(person);
+        return MessageResponseDTO.builder()
+                .message("Created person with ID " + savedPerson.getId())
+                .build();
+    }
+}
