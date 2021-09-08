1723ae0bb6d874f59a10b00a2ce2958b21ffda0a
Maike Rodrigues <maikedev7@gmail.com>
2021-09-07 21:31:13 -0300

    Improved person creation functionality

--- src/main/java/com/example/personapi/service/PersonService.java
+++ src/main/java/com/example/personapi/service/PersonService.java
@@ -1,21 +1,27 @@
 package com.example.personapi.service;
 
+import com.example.personapi.dto.request.PersonDTO;
 import com.example.personapi.dto.response.MessageResponseDTO;
 import com.example.personapi.entity.Person;
+import com.example.personapi.mapper.PersonMapper;
 import com.example.personapi.repository.PersonRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonService {
 
     private PersonRepository personRepository;
+    public final PersonMapper personMapper = PersonMapper.INSTANCE;
 
-    public PersonService(PersonRepository personRepository) {
-        this.personRepository = personRepository;
-    }
+    public MessageResponseDTO createPerson(PersonDTO personDTO){
 
-    public MessageResponseDTO createPerson(Person person){
-        Person savedPerson = this.personRepository.save(person);
+        Person personToSave = this.personMapper.toModel(personDTO);
+
+        Person savedPerson = this.personRepository.save(personToSave);
+
         return MessageResponseDTO.builder()
                 .message("Created person with ID " + savedPerson.getId())
                 .build();
